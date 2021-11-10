c6545a70c02d94e95b6c31b90796c88efe356f1d
salvamundi <fjsociale39@gmail.com>
2021-11-10 01:54:07 +0100

    v0.1.1 Created some basics window variables. Still need to define more. v0.1 Initial commit

--- src/ScreenDrawer.py
+++ src/ScreenDrawer.py
@@ -1,6 +1,3 @@
-from tkinter import *
-win = Tk()
-s_wdth = win.winfo_screenwidth()
-s_hght = win.winfo_screenheight()
-print(f"{s_wdth}x{s_hght}")
-win.mainloop()
+import canvas
+
+canvas = canvas.Canvas()
--- src/canvas.py
+++ src/canvas.py
@@ -1,5 +1,17 @@
+from tkinter import *
 
-class canvas():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+
+class Canvas:
+    def __init__(self):
+        window = Tk()
+        window.title("ScreenDrawer") # title actually is not necessary since I want the window frames to be invisible
+
+        w_wdth = window.winfo_screenwidth() # get screen width
+        w_hgth = window.winfo_screenheight() # get screen height
+
+        # I use entire screen dimensions since the canvas that I want to write on
+        # must be on entire screen
+        window.geometry(f"{w_wdth}x{w_hgth}") # geometry(widthxheight)
+        window.attributes("-alpha", 0.1) # this sets window transparency
+
+        window.mainloop()
