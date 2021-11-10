3e483144069e9c404f1bb8badd2068ce8d7d3100
salvamundi <fjsociale39@gmail.com>
2021-11-10 01:08:45 +0100

    v0.1 Initial commit

---
+++ src/ScreenDrawer.py
@@ -0,0 +1,6 @@
+from tkinter import *
+win = Tk()
+s_wdth = win.winfo_screenwidth()
+s_hght = win.winfo_screenheight()
+print(f"{s_wdth}x{s_hght}")
+win.mainloop()
---
+++ src/canvas.py
@@ -0,0 +1,5 @@
+
+class canvas():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
